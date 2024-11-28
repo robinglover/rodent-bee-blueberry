066076cd1d3fb148066370fef4b91b8ebb93c67d
robinglover <robin.glover@zoology.ubc.ca>
2024-11-28 12:51:20 -0800

    annotate

--- 01_scripts/data-rounds-analysis.R
+++ 01_scripts/data-rounds-analysis.R
@@ -1,3 +1,5 @@
+#####SETUP#####
+
 #load packages
 library(ggplot2)
 library(dplyr)
@@ -14,6 +16,8 @@ round_data1 <- round_data %>%
   rename(date = Date) %>% #change date column name to "julien.date" 
   rename (field.type = grass) #change date column name to "julien.date" 
 
+#####FIXING UP DATAFRAME######
+
 #change field type to a factor
 round_data1$field.type <- as.factor(round_data1$field.type)
 
@@ -26,32 +30,44 @@ round_data1$date <- as.Date(round_data1$date)
 #add column for julian date
 round_data1$julian.date <- yday(round_data1$date)
 
+#####CREATE FILTERED DATAFRAMES#####
+
 #filter to only vegetation surveys
 veg_round_data <- filter(round_data1, Survey.type == "vegetation (rodent)" | Survey.type == "vegetation (bee)" | Survey.type == "vegetation (bee and rodent)")
-
-#plot date vs field type for veg data
-ggplot(veg_round_data, aes(y=field.type, x=julian.date))+ geom_boxplot()
-
-#plot date vs field type, grouped by area
-ggplot(veg_round_data, aes(y=area, x=julian.date, colour = field.type))+ geom_boxplot()
 
 #filter to only bee surveys
 bee_round_data <- filter(round_data1, Survey.type == "bombus")
 
-#plot date vs field type for bee data
-ggplot(bee_round_data, aes(y=field.type, x=julian.date))+ geom_boxplot()
-
-#plot date vs field type, grouped by area
-ggplot(bee_round_data, aes(y=area, x=julian.date, colour = field.type))+ geom_boxplot()
-
 #filter to only rodent surveys
 rodent_round_data <- filter(round_data1, Survey.type == "rodent (holes only)" | Survey.type == "rodent (powder)")
 
+#####PLOTS#####
+
+##VEG
+
 #plot date vs field type for veg data
+ggplot(veg_round_data, aes(y=field.type, x=julian.date))+ geom_boxplot()
+
+#plot date vs field type for veg data, grouped by area
+ggplot(veg_round_data, aes(y=area, x=julian.date, colour = field.type))+ geom_boxplot()
+
+##BEE
+
+#plot date vs field type for bee data
+ggplot(bee_round_data, aes(y=field.type, x=julian.date))+ geom_boxplot()
+
+#plot date vs field type for bee data, grouped by area
+ggplot(bee_round_data, aes(y=area, x=julian.date, colour = field.type))+ geom_boxplot()
+
+##RODENT
+
+#plot date vs field type for rodent data
 ggplot(rodent_round_data, aes(y=field.type, x=julian.date))+ geom_boxplot()
 
-#plot date vs field type, grouped by area
+#plot date vs field type for rodent data, grouped by area
 ggplot(rodent_round_data, aes(y=area, x=julian.date, colour = field.type))+ geom_boxplot()
 
-#
+####STATS ANALYSIS####
+
+#Attempting
 z <- lm(julian.date ~ field.type + (1|area), data = round_data1)
