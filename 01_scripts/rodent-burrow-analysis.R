21944082d75cdf5278eae67df6cddb3cdbccdb3a
robinglover <robin.glover@zoology.ubc.ca>
2024-11-28 12:48:01 -0800

    annotate R script

--- 01_scripts/rodent-burrow-analysis.R
+++ 01_scripts/rodent-burrow-analysis.R
@@ -1,32 +1,39 @@
+######SETUP#######
+
 #load packages
-library(ggplot2)
-library(dplyr)
-library(tidyr)
+library(ggplot2) #for plots
+library(dplyr) #for working with dataframes
+library(tidyr) #for working with datasets
 
 #read csv
-rodent <- read.csv("00_rawdata/rodent-data.csv")
-site_data <- read.csv("00_rawdata/site-info.csv")
+rodent <- read.csv("00_rawdata/rodent-data.csv") #rodent burrow data
+site_data <- read.csv("00_rawdata/site-info.csv") #site data
 
-#add column to round data for site type (either grass or bare)
+#add column to round data for site type (either grass or bare) by joining 
+#rodent dataframe to site dataframe
 rodent1 <- rodent %>%
   left_join(site_data, by = c("site_id" = "name")) %>%
-  rename (field.type = grass) #change date column name to "julien.date" 
+  rename (field.type = grass) #column listing field condition (either bare or 
+                              #grass)  to "field.type" 
 
+#####FILTERED DATAFRAMES########
 
-#filter to only holes with certain IDs
-rodent_certain <- filter(rodent1, species_guess_final == "dm" | species_guess_final == "v")
+#filter dataframe to only contain holes with certain IDs (either vole or dm)
+rodent_certain_species <- filter(rodent1, species_guess_final == "dm" | species_guess_final == "v")
 
-#plot hole diameter vs species guess
-ggplot(rodent_certain, aes(y=diameter_cm, x=species_guess_final))+ geom_boxplot()
+#filter dataframe to only include holes with certain IDs for activity (either yes or no)
+rodent_certain_activity <- filter(rodent1, powder_active_final == "y" | powder_active_final == "n")
 
-#filter to only holes with certain IDs for activity
-rodent_certain2 <- filter(rodent1, powder_active_final == "y" | powder_active_final == "n")
+#####PLOTS######
 
-#plot hole diameter vs species guess
-ggplot(rodent_certain2, aes(x=field.type, fill=powder_active_final))+ geom_bar(position="dodge", stat="count")
+#plot hole diameter vs species guess (for certain species guesses)
+ggplot(rodent_certain_species, aes(y=diameter_cm, x=species_guess_final))+ geom_boxplot()
 
-#plot hole diameter vs species guess
-ggplot(rodent_certain, aes(x=field.type, fill=species_guess_final))+ geom_bar(position="dodge", stat="count")
+#plot number of active and inactive burrows in each field type
+ggplot(rodent_certain_activity, aes(x=field.type, fill=powder_active_final))+ geom_bar(position="dodge", stat="count")
 
-#plot hole diameter vs species guess
+#plot number of active burrows of each species (v or dm) in each field type
+ggplot(rodent_certain_species, aes(x=field.type, fill=species_guess_final))+ geom_bar(position="dodge", stat="count")
+
+#plot number of active burrows of all species guesses (v,dm, unknown) in each field type
 ggplot(rodent1, aes(x=field.type, fill=species_guess_final))+ geom_bar(position="dodge", stat="count")
