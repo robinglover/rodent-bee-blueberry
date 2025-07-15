39046978e46ed53a5181ab21ca8e69c36cc2de10
robinglover <robin.glover@zoology.ubc.ca>
2025-07-15 12:44:44 -0700

    updating script to add floral species from my data to useful flowers list

--- 01_scripts/creating_floral_list.R
+++ 01_scripts/creating_floral_list.R
@@ -3,45 +3,79 @@ library(stringr)
 
 #load Jenna's floral data
 jenna_flowers <- read.csv("06_cleandata/floral_interaction_data/bombusflowers_Jenna.csv")
+jenna_flowers <-jenna_flowers%>%
+  mutate(flower_species = str_replace_all(tolower(V1), " ", "_"))
 
 #load Duncan's and Carly's data
 DC_flowers <- read.csv("06_cleandata/floral_interaction_data/bombus_flower_Duncan_Carly.csv")
 DC_flowers <- rename(DC_flowers, V1 = plant_sp)
-
-# Filter rows in DC_only where V1 is not already in jenna_flowers
-new_rows <- DC_flowers%>%
-  filter(!V1 %in% jenna_flowers$V1)
-
-# Bind those rows in DC_only to jenna_flowers
-JDC_flowers <- bind_rows(jenna_flowers, new_rows)
-
-#create new column with proper species name format 
-JDC_flowers <- JDC_flowers %>%
+DC_flowers <-DC_flowers%>%
   mutate(flower_species = str_replace_all(tolower(V1), " ", "_"))
 
+#create list of flowers that bees visited in my study
+robin_floral <- read.csv("06_cleandata/flowers_CLEAN.csv")
+robin_flower_species <- unique(robin_floral$flower_species)
+#Convert to data frame first
+robin_flowers <- data.frame(flower_species = robin_flower_species)
+
+# Filter rows in DC_flowers where flower_species is not already in jenna_flowers
+new_rows_DC <- DC_flowers%>%
+  filter(!flower_species %in% jenna_flowers$flower_species)
+
+# Bind those rows to jenna_flowers
+JDC_flowers <- bind_rows(jenna_flowers, new_rows_DC)
+
+# Filter rows in robin_flowers where flower_species is not already in JDC_flowers
+new_rows_rg <- robin_flowers%>%
+  filter(!flower_species %in% JDC_flowers$flower_species)
+
+#bind rows to JDC_flowers
+JDCR_flowers <- bind_rows(JDC_flowers, new_rows_rg)
+
+#replace spaces with _ if needed
+JDCR_flowers <- JDCR_flowers %>%
+  mutate(flower_species = str_replace_all(flower_species, " ", "_"))
+
 #create new column for species code
-JDC_flowers <- JDC_flowers %>%
+JDCR_flowers <- JDCR_flowers %>%
   mutate(
     flower_code = str_to_lower(
       str_c(
-        str_sub(word(V1, 1), 1, 2),
-        str_sub(word(V1, 2), 1, 2)
+        str_sub(word(flower_species, 1, sep = "_"), 1, 2),
+        str_sub(word(flower_species, 2, sep = "_"), 1, 2)
       )
     )
   )
 
 #only save useful columns
-JDC_flowers <- JDC_flowers %>%
+JDCR_flowers <- JDCR_flowers %>%
   select(flower_species, flower_code)
 
-#write to new csv
-write_csv(JDC_flowers, file = file.path("06_cleandata","useful_flowers.csv"))
+#remove "ask tyler" row
+JDCR_flowers <- JDCR_flowers %>%
+  filter(flower_species != "ask_tyler")
 
-#all floral genera that are generally used by bees (include all species under these genera in analyses)
-##rhododendron_spp.
-##rorippa_spp.
-##rubus_spp.
+#remove duplicate rows
+JDCR_flowers <- JDCR_flowers %>%
+  distinct(flower_species, .keep_all = TRUE)
+
+#change spp. to sp in flower species
+JDCR_flowers <- JDCR_flowers %>%
+  mutate(flower_species = str_replace_all(flower_species, "_spp\\.?$", "_sp"))
+
+#write to new csv
+write_csv(JDCR_flowers, file = file.path("06_cleandata","useful_flowers.csv"))
+
+#find list of all floral genera (sp. or spp.)
+problematic_species <- JDCR_flowers %>%
+  filter(str_detect(flower_code, "sp"))
+
+#floral genera that are generally used by bees (include all species under these genera in analyses)
+##rhododendron_sp
+##rorippa_sp
+##rubus_sp
 ##rosa_sp
 ##calystegia_sp
 ##prunus_sp
 ##phacelia_sp
+##brassicaceae_sp
